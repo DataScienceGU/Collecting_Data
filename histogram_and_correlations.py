4791a4b16bf49988cb53ceb2a82986786a8f307d
Karina Chan <kjc83@georgetown.edu>
2019-11-03 22:13:40 -0500

    saving to merge w master

--- histogram_and_correlations.py
+++ histogram_and_correlations.py
@@ -5,6 +5,7 @@ import numpy as np
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
+import requests
 
 pd.set_option('display.max_columns', 20)
 pd.set_option('display.width', 1000)
@@ -27,7 +28,21 @@ def load_gun_sheets():
 
     return all_shooting_data
 
-def plot_histograms(shooting_data):
+def getGenres():
+    genreDF = pd.DataFrame()
+    baseURL = "https://api.themoviedb.org/3/genre/movie/list"
+    queryString = {'api_key': 'ddb02b46eef4bf31dc4e8bcbc0222a68',
+                   'language': 'en-US'}
+    response = requests.get(baseURL, queryString)
+    df = response.json()
+
+    genres = df['genres']
+    genreDF = genreDF.append(genres, ignore_index=True)
+
+    genreDF.to_csv("movie_genres.csv")
+    print(genreDF)
+
+def plot_shooting_histograms(shooting_data):
     #using date as last histogram value from this database, separating month over month per year
     #i figured day by day was too granular.
     shooting_data['date'] = shooting_data['date'].astype('str')
@@ -42,7 +57,8 @@ def plot_histograms(shooting_data):
     # shooting_data['month_year'] = shooting_data['month_year'].astype('category').cat.codes
 
     print(shooting_data)
-    shooting_data.hist()
+    # shooting_data.hist()
+    shooting_data["month_year"].hist()
     plt.show()
     plt.clf()
     plt.close()
@@ -68,16 +84,41 @@ def plot_histograms(shooting_data):
     plt.clf()
     plt.close()
 
+def plot_movie_histograms(movie_data):
+    # Find the correlation between all the pairs of these quantity variables.
+    # Include a table of the output in your report, and explain your findings – what does this indicate about your data?
+    # Use scatterplots to display the results. Ideally, create a set of scatterplot subplots.
+    keywords = []
+    violent_words = ['gun', 'shoot', 'murder']
+    for movie in movie_data['overview']:
+        # print(movie)
+        words = []
+        for word in violent_words:
+            if word in movie:
+                words.extend(word)
+
+        keywords.append(words)
+
+    movie_data['keywords'] = keywords
+    movie_data['keywords'].value_counts().plot(kind='bar')
+    plt.show()
+
+
+
 
 def main():
     shooting_data = load_gun_sheets()
+    movie_data = pd.read_csv("all_movies.csv")
+
+
     print(shooting_data)
     print("killed max: ")
     print(shooting_data['killed'].max())
     print("wounded max: ")
 
     print(shooting_data['wounded'].max())
-    plot_histograms(shooting_data)
+    plot_shooting_histograms(shooting_data)
+    plot_movie_histograms(movie_data)
 
 
 
