cb5f67a7622587761204a570f75df64e84a0f9a6
Karina Chan <kjc83@georgetown.edu>
2019-12-09 18:10:15 -0500

    Adding manually labeled movie data and sentiment analysis

--- sentiment_analysis_and_visualizations.py
+++ sentiment_analysis_and_visualizations.py
@@ -16,6 +16,11 @@ import chart_studio
 import chart_studio.plotly as py
 import chart_studio.tools as tls
 from plotly.subplots import make_subplots
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+
 
 pd.set_option('display.max_columns', 20)
 pd.set_option('display.width', 1000)
@@ -191,21 +196,38 @@ def visualize_bubble_chart(movie_data):
         date = date.date()
 
     # getting rid of data before the shooting data
-    start_date = '2013-1-1'
-    mask=(bubble_movie_data['release_date']>start_date)
-    bubble_movie_data=bubble_movie_data.loc[mask]
+    # start_date = '2000-1-1'
+    # mask=(bubble_movie_data['release_date']>start_date)
+    # bubble_movie_data=bubble_movie_data.loc[mask]
+
+    #removing outliers
+    remove_outlier(bubble_movie_data, "vote_average")
 
     hover_text = []
     # adding combined text column for hover text labels:
     for index, row in bubble_movie_data.iterrows():
         hover_text_string = row['original_title'] + " violent words: " + str(row['violent_words'])
-        hover_text_string += " peaceful words: " + str(row['peaceful_words'])
-        print(hover_text_string)
+        hover_text_string += "\n peaceful words: " + str(row['peaceful_words'])
+        hover_text_string += "\n number of votes: " + str(row['vote_count'])
         hover_text.append(hover_text_string)
 
     bubble_movie_data['hover_text'] = hover_text
 
     print(bubble_movie_data)
+    blayout = go.Layout(title=go.layout.Title(
+        text='Popularity and Violence Ratings of Movies Released between 2013-Present',),
+        xaxis=go.layout.XAxis(
+            title=go.layout.xaxis.Title(
+                text='Release Date'
+            )
+        ),
+        yaxis=go.layout.YAxis(
+            title=go.layout.yaxis.Title(
+                text='Voter Rating'
+                )
+            )
+        )
+
     fig = go.Figure(data=[go.Scatter(
         x=bubble_movie_data['release_date'],
         y=bubble_movie_data['vote_average'],
@@ -220,27 +242,122 @@ def visualize_bubble_chart(movie_data):
             colorbar=dict(title="Violence Rating",
                           titleside="top",
                           tickmode="array",
-                          tickvals=[min(bubble_movie_data['violence_rating']), 0, max(bubble_movie_data['violence_rating'])],
+                          tickvals=[min(bubble_movie_data['violence_rating']),
+                                    (sum(bubble_movie_data['violence_rating'])/len(bubble_movie_data['violence_rating'])),
+                                    max(bubble_movie_data['violence_rating'])],
                           ticktext=["Peaceful", "Neutral", "Violent"],
                           ticks="outside"
                           )
             # color_continuous_scale=bubble_movie_data['violence_rating']
         )
-    )])
-    layout = go.Layout
+    )], layout = blayout)
+
 
     fig.show()
     # py.plot(fig, filename='bubble-graph', auto_open=True)
 
 # from: https://datascience.stackexchange.com/questions/33632/remove-local-outliers-from-dataframe-using-pandas
-# def remove_outlier(df_in, col_name):
-#     q1 = df_in[col_name].quantile(0.25)
-#     q3 = df_in[col_name].quantile(0.75)
-#     iqr = q3-q1 #Interquartile range
-#     fence_low  = q1-1.5*iqr
-#     fence_high = q3+1.5*iqr
-#     df_out = df_in.loc[(df_in[col_name] > fence_low) & (df_in[col_name] < fence_high)]
-#     return df_out
+def remove_outlier(df_in, col_name):
+    q1 = df_in[col_name].quantile(0.25)
+    q3 = df_in[col_name].quantile(0.75)
+    iqr = q3-q1 #Interquartile range
+    fence_low  = q1-1.5*iqr
+    fence_high = q3+1.5*iqr
+    df_out = df_in.loc[(df_in[col_name] > fence_low) & (df_in[col_name] < fence_high)]
+    return df_out
+
+def perform_sentiment_analysis(movie_data):
+    q1 = movie_data['violence_rating'].quantile(0.33)
+    q3 = movie_data['violence_rating'].quantile(0.66)
+
+    violence_classification = []
+    for index, row in movie_data.iterrows():
+        if(row['violence_rating']<q1):
+            violence_classification.append("peaceful")
+        elif(row['violence_rating']>q3):
+            violence_classification.append("violent")
+        else:
+            violence_classification.append("neutral")
+
+    movie_data['violence_classification']=violence_classification
+    print(movie_data)
+    print(movie_data['violence_classification'].value_counts())
+    # movie_data.to_csv("manually_labeled_movies.csv")
+    run_naive_bayes(movie_data, movie_data['violence_classification'])
+
+def run_naive_bayes(movies, classes):
+    # set of labeled data (X_validate, Y_validate)
+    myData = movies.copy()
+    myData = myData.drop(columns=["Unnamed: 0", "adult", "genre_ids", "id", "original_language", "original_title", "popularity",
+                         "release_date", "title", "video", "overview", "vote_average", "vote_count", "violent"])
+    print(myData)
+    valueArray = myData.values
+    lastColNum = len(list(myData))-1
+    X = valueArray[:, 0:lastColNum]
+    Y = classes
+    test_size = 0.20
+    seed = 7
+    X_train, X_validate, Y_train, Y_validate = train_test_split(X, Y, test_size=test_size, random_state=seed)
+
+    # Setup 10-fold cross validation to estimate the accuracy of different models
+    # Split data into 10 parts
+    # Test options and evaluation metric
+    num_folds = 10
+    seed = 7
+    scoring = 'accuracy'
+
+    ######################################################
+    # Use different algorithms to build models
+    ######################################################
+
+    # Add each algorithm and its name to the model array
+    model= GaussianNB()
+
+    # Evaluate each model, add results to a results array,
+    # Print the accuracy results (remember these are averages and std
+    kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=False)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    msg = "%s: %f (%f)" % ("GAUSS", cv_results.mean(), cv_results.std())
+    print(msg)
+
+def run_manually_labeled_naive_bayes():
+    my_Data = pd.read_csv("manually_labeled_movies.csv")
+    print("IN MANUALLY LABELED DATA")
+    print(my_Data)
+
+    my_Data = my_Data.drop(
+        columns=["Unnamed: 0", "Unnamed: 0.1","adult", "genre_ids", "id", "original_language", "original_title", "popularity",
+                 "release_date", "title", "video", "overview", "vote_average", "vote_count", "violent"])
+    print(my_Data)
+    valueArray = my_Data.values
+    lastColNum = len(list(my_Data)) - 1
+    X = valueArray[:, 0:lastColNum]
+    Y = my_Data['violence_rating']
+    test_size = 0.20
+    seed = 7
+    X_train, X_validate, Y_train, Y_validate = train_test_split(X, Y, test_size=test_size, random_state=seed)
+
+    # Setup 10-fold cross validation to estimate the accuracy of different models
+    # Split data into 10 parts
+    # Test options and evaluation metric
+    num_folds = 10
+    seed = 7
+    scoring = 'accuracy'
+
+    ######################################################
+    # Use different algorithms to build models
+    ######################################################
+
+    # Add each algorithm and its name to the model array
+    model = GaussianNB()
+
+    # Evaluate each model, add results to a results array,
+    # Print the accuracy results (remember these are averages and std
+    kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=False)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    msg = "%s: %f (%f)" % ("GAUSS", cv_results.mean(), cv_results.std())
+    print(msg)
+
 
 def process_movie_date(movie_data):
     month_year=[]
@@ -289,9 +406,6 @@ def process_movie_date(movie_data):
     movie_data_genre = pd.DataFrame(list(sensational_genres_per_month.items()), columns = ['date', 'num_movies_in_violent_genres'])
     movie_data_2013_on =  pd.merge(movie_data_violence, movie_data_genre, on='date', how = 'outer')
 
-
-    #printing merged data:
-    print(movie_data_2013_on)
     return movie_data_2013_on
 
 def remove_movies_before_shootings(violent_movies_per_month):
@@ -311,19 +425,14 @@ def main():
     shooting_data = load_gun_sheets()
     movie_data = pd.read_csv("all_movies.csv")
 
-    #Max values for killed and wounded in shooting history:
-    print(shooting_data)
-    print("killed max: ")
-    print(shooting_data['killed'].max())
-    print("wounded max: ")
-    print(shooting_data['wounded'].max())
-
     #preprocessing: formating the month and year
     format_shooting_data(shooting_data)
 
     #showing stacked bar chart data for shootings and violent movies released in the same month
     movie_data_unseperated = format_movie_data(movie_data)
+    perform_sentiment_analysis(movie_data)
     visualize_bubble_chart(movie_data_unseperated)
+    run_manually_labeled_naive_bayes()
     movie_and_shooting = stacked_histogram(shooting_data, movie_data_unseperated)
 
 if __name__ == "__main__":
